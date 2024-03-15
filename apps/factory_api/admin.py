8f1949590427e7db3d9c59e3060c2a7b4224a864
JeyJey871 <ginza@tuit.uz>
2024-03-16 02:31:01 +0500

    tuning

--- apps/factory_api/admin.py
+++ apps/factory_api/admin.py
@@ -1,22 +1,34 @@
 from django.contrib import admin
 from .models import Product, Material, ProductMaterial, Warehouse
 
+
 class ProductMaterialInline(admin.TabularInline):
     model = ProductMaterial
-    extra = 1
+    extra = 2
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
     inlines = [ProductMaterialInline]
+
+
+class WarehouseInline(admin.TabularInline):
+    model = Warehouse
+    extra = 2
+
+
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    inlines = [WarehouseInline]
+
 
 @admin.register(ProductMaterial)
 class ProductMaterialAdmin(admin.ModelAdmin):
     list_display = ('product', 'material', 'quantity')
 
+
 @admin.register(Warehouse)
 class WarehouseAdmin(admin.ModelAdmin):
-    list_display = ('material', 'remainder','for_check_remainder_count', 'price')
+    list_display = ('material', 'remainder', 'for_check_remainder_count', 'price')
