82ff1d4fd7484b911f17ecf7fb71735883b6671e
JeyJey871 <ginza@tuit.uz>
2024-03-16 02:40:16 +0500

    tuning

--- core/generator.py
+++ core/generator.py
@@ -4,5 +4,5 @@ from drf_yasg.generators import OpenAPISchemaGenerator
 class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
-        schema.schemes = ["http", "https"]
+        schema.schemes = ["https"]
         return schema
