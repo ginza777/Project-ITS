9ccefe71f2eb05f6de227b3dcc35b336dbaaf497
JeyJey871 <ginza@tuit.uz>
2024-03-16 02:18:42 +0500

    its

--- apps/factory_api/models.py
+++ apps/factory_api/models.py
@@ -1,22 +1,48 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=50)
 
+    def save(self, *args, **kwargs):
+        self.code = str(uuid.uuid4())
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name_plural = 'product'
+        verbose_name = 'product'
+
 
 class Material(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+    class Meta:
+        db_table = 'material'
+        verbose_name_plural = 'material'
+        verbose_name = 'material'
+
 
 class ProductMaterial(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     quantity = models.FloatField()
+
+    def __str__(self):
+        return str(self.product.name)
+
+    class Meta:
+        db_table = 'product_material'
+        verbose_name_plural = 'product_material'
+        verbose_name = 'product_material'
+
 
 class Warehouse(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
@@ -25,4 +51,9 @@ class Warehouse(models.Model):
     price = models.FloatField()
 
     def __str__(self):
-        return f"{self.material.name} - {self.remainder}"
+        return str(self.material.name)
+
+    class Meta:
+        db_table = 'warehouse'
+        verbose_name_plural = 'warehouse'
+        verbose_name = 'warehouse'
